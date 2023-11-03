ced5faf8f81c983a7ff70d64561a0f3af7a9cd04
benny <bennycortese@gmail.com>
2023-11-02 23:53:31 -0400

    Added openai import and some calling, need to add api key and get this actually running

--- converse_main.py
+++ converse_main.py
@@ -1,12 +1,21 @@
 import speech_recognition as sr
+import openai
 
-#https://www.analyticsvidhya.com/blog/2021/12/guide-for-speech-recognition/ -- example tutorial
+# https://www.analyticsvidhya.com/blog/2021/12/guide-for-speech-recognition/ -- example tutorial
 
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are having a conversation"},
+            {"role": "user", "content": audio},
+        ]
+    )
+
 
 # write audio to a WAV file
 with open("microphone-results.wav", "wb") as f:
