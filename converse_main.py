5da3403665241d298c48f5e20a6deea49a7f1c2e
pizza654321 <bennycortese@gmail.com>
2023-11-18 23:48:18 -0500

    Function for asking the user what language they are speaking it and storing that data

--- converse_main.py
+++ converse_main.py
@@ -9,6 +9,8 @@ r = sr.Recognizer()
 
 chatting = True
 
+language = language_setter()
+
 while chatting:
 	with sr.Microphone() as source:
     	print("Talk with the bot!")
@@ -26,3 +28,17 @@ while chatting:
 # write audio to a WAV file
 with open("microphone-results.wav", "wb") as f:
     f.write(audio.get_wav_data())
+
+def language_setter():
+	with sr.Microphone() as source:
+    	print("What language are you speaking?")
+    	audio = r.listen(source)
+        response = openai.ChatCompletion.create(
+        	model="gpt-3.5-turbo",
+        	messages=[
+            	{"role": "system", "content": "You will tell the user what language they are speaking"},
+            	{"role": "user", "content": audio},
+        	]
+    	)
+    language = response
+    return language
